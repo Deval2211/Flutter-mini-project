d88c367f12b84f3e75d2d34ce10a2772a1652c29
Deval Shah <150435571+Deval2211@users.noreply.github.com>
2025-11-22 13:11:39 +0530

    Dynamic TODO App

---
+++ practical_3/lib/main.dart
@@ -0,0 +1,33 @@
+//PRACTICAL 3 : Multiscreen Application Login and dashboard and profile and
+//                Temperature convertor app
+import 'package:flutter/material.dart';
+import 'login.dart';
+import 'dashboard.dart';
+import 'profile.dart';
+import 'todolist.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => const LoginScreen(),
+        '/dashboard': (context) => const DashboardScreen(),
+        '/profile': (context) => const ProfileScreen(),
+        '/todolist': (context) => const TodoListScreen(),
+      },
+    );
+  }
+}
