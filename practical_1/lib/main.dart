a976f71e21f4a8631593ccfb5cfd29e912ba01df
Deval Shah <150435571+Deval2211@users.noreply.github.com>
2025-11-22 13:08:36 +0530

    Basic Multi-Screen App

---
+++ practical_1/lib/main.dart
@@ -0,0 +1,30 @@
+//PRACTICAL 1 : Multiscreen Application Login and dashboard and profile
+import 'package:flutter/material.dart';
+import 'login.dart';
+import 'dashboard.dart';
+import 'profile.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => const LoginPage(),
+        '/dashboard': (context) => const DashBoardPage(),
+        '/profile': (context) => const ProfilePage(),
+      },
+    );
+  }
+}
