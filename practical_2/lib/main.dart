303b8b0b79e3278464833a5ef1c32c313f2c29ba
Deval Shah <150435571+Deval2211@users.noreply.github.com>
2025-11-22 13:10:36 +0530

    Temperature Converter App

---
+++ practical_2/lib/main.dart
@@ -0,0 +1,31 @@
+//PRACTICAL 2 : Multiscreen Application Login and dashboard and profile and
+//                Temperature convertor app
+import 'package:flutter/material.dart';
+import 'login.dart';
+import 'dashboard.dart';
+import 'profile.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => const LoginPage(),
+        '/dashboard': (context) => const DashBoardPage(),
+        '/profile': (context) => const ProfilePage(),
+      },
+    );
+  }
+}
